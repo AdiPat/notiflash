52713fe302b7b61b89413ac1cda47633c234addb
AdiPat <contact.adityapatange@gmail.com>
2024-05-26 13:20:14 +0530

    add dotenv support

--- server/main.go
+++ server/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
 	"main/handlers"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	http.HandleFunc("/", handlers.HelloHandler)
 	http.HandleFunc("/notification", handlers.NotificationHandler)
 	http.ListenAndServe(":8080", nil)
